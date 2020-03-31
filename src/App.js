7b7163e0a3045c4a357b9684c20abe2bd250cd4d
Code-Hello <m.tuto@protonmail.com>
2020-03-31 13:39:52 +0200

    props exercise solution

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Contact from './components/Contact.js';
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Contact name="Terri Lowe" avatar="https://randomuser.me/api/portraits/women/23.jpg" online />
+      <Contact name="Ramon Hunt" avatar="https://randomuser.me/api/portraits/men/4.jpg" />
+      <Contact name="Mildred Mendoza" avatar="https://randomuser.me/api/portraits/women/54.jpg" online />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Contact.css';
 
-const Contact = () => {
+const Contact = ({name, avatar, online}) => {
+    console.log(online)
     return (
         <div className="Contact">
             <div>
-                <img className="avatar" src="https://randomuser.me/api/portraits/men/81.jpg" alt="user's avatar"/>
+                <img className="avatar" src={avatar} alt="user's avatar"/>
             </div>
             <div>
-                <h4 className="name">Joe Rodriguez</h4>
+                <h4 className="name">{name}</h4>
                 <div className="status">
-                    <span className="status-online"></span>
-                    <p className="status-text">online</p>
+                    <span className={online ? 'status-online' : 'status-offline'}></span>
+                    <p className="status-text">{online ? 'online' : 'offline'}</p>
                 </div>
             </div>
         </div>
     );
+}
+
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    online: PropTypes.bool
 }
 
 export default Contact;
